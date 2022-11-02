ad952825a1ac59dcf1d2177f516cfb2aa5b3b841
Fatima Ponce <faatii.ponce@gmail.com>
2022-11-02 10:22:41 -0300

    Backend

--- api/src/routes/dogRoute.js
+++ api/src/routes/dogRoute.js
@@ -1,87 +1,95 @@
-const {getAllDogs} = require('./toShowApi')
-const { Dog, Temper } = require('../db')
-const {API_KEY}= process.env
-const axios = require('axios')
-const { Router } = require('express');
+const { getAllDogs } = require("./toShowApi");
+const { Dog, Temper } = require("../db");
+const { API_KEY } = process.env;
+const axios = require("axios");
+const { Router } = require("express");
 const router = Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const {name} = req.query;
+router.get("/", async (req, res) => {
+  try {
+    const { name } = req.query;
     const allDogs = await getAllDogs();
-    // console.log(name)
-    if(name) {
-        let dog = allDogs.filter((ele) => 
-            name.toLowerCase().includes(ele.name.toLowerCase())
-        )
-       return dog.length
-        ? res.status(201).send(dog)
-        : res.status(404).send('Perro no encontrado')
-    } else {
-        return res.status(201).send(allDogs)
-    }
-    } catch (error) {
-        res.status(404).send(`Error: ${error}`)
-    }
-})
 
-router.get('/:idRaza', async (req,res) => {
-    try {const {idRaza} = req.params;
+    if (name) {
+      let dog = allDogs.filter((ele) =>
+        name.toLowerCase().includes(ele.name.toLowerCase())
+      );
+      return dog.length
+        ? res.status(201).send(dog)
+        : res.status(404).send("Perro no encontrado");
+    } else {
+      return res.status(201).send(allDogs);
+    }
+  } catch (error) {
+    res.status(404).send(`Error: ${error}`);
+  }
+});
+
+router.get("/:idRaza", async (req, res) => {
+  try {
+    const { idRaza } = req.params;
     const allDogs = await getAllDogs();
     let dog = allDogs.filter((elem) => Number(elem.id) === Number(idRaza));
     dog
-    ? res.status(201).send(dog)
-    : res.status(404).send('Raza no encontrada')
-    } catch (error) {
-        res.status(404).send(`Error: ${error}`)
-    }
-})
+      ? res.status(201).send(dog)
+      : res.status(404).send("Raza no encontrada");
+  } catch (error) {
+    res.status(404).send(`Error: ${error}`);
+  }
+});
 
-router.post('/', async (req, res) => {
-    try {
-        const {name, min_height, max_height, min_weight, max_weight, years, temperaments, image} = req.body
-        
-        const heightArray = [];
-        const minHeight = min_height;
-        const maxHeight = max_height;
-        heightArray.push(minHeight, maxHeight);
+router.post("/", async (req, res) => {
+  try {
+    const {
+      name,
+      min_height,
+      max_height,
+      min_weight,
+      max_weight,
+      years,
+      temperaments,
+      image,
+    } = req.body;
 
-        const weightArray = [];
-        const minWeight = min_weight;
-        const maxWeight = max_weight;
-        weightArray.push(minWeight, maxWeight);
+    const heightArray = [];
+    const minHeight = min_height;
+    const maxHeight = max_height;
+    heightArray.push(minHeight, maxHeight);
 
-        let perrito = await Dog.create({
-            name,
-            height: heightArray,
-            weight: weightArray,
-            years,
-            image: image ? image : `https://i.pinimg.com/474x/30/d0/31/30d031cbb901cf4ca083555e10271ee5.jpg`
-        })
-        let concaTemp = await Temper.findAll({
-            where: {name: temperaments}
-        })
-        perrito.addTemper(concaTemp);
-        return res.status(201).send("Perrito creado con éxito")
-    } catch (error) {
-        res.status(404).send(`Error: ${error}`)
-    }
-})
+    const weightArray = [];
+    const minWeight = min_weight;
+    const maxWeight = max_weight;
+    weightArray.push(minWeight, maxWeight);
 
+    let perrito = await Dog.create({
+      name,
+      height: heightArray,
+      weight: weightArray,
+      years,
+      image: image
+        ? image
+        : `https://i.pinimg.com/474x/30/d0/31/30d031cbb901cf4ca083555e10271ee5.jpg`,
+    });
+    let concaTemp = await Temper.findAll({
+      where: { name: temperaments },
+    });
+    perrito.addTemper(concaTemp);
+    return res.status(201).send("Perrito creado con éxito");
+  } catch (error) {
+    res.status(404).send(`Error: ${error}`);
+  }
+});
 
-router.post('/delete/:name', async (req, res) => {
-    try {
-        const {name} = req.params;
-        const result = await Dog.destroy({
-            where: {name:`${name}`}
+router.post("/delete/:name", async (req, res) => {
+  try {
+    const { name } = req.params;
+    const result = await Dog.destroy({
+      where: { name: `${name}` },
+    });
+    res.status(201).send("Perro eliminado con éxito");
+  } catch (error) {
+    res.status(404).send(`Error al eliminar: ${error}`);
+  }
+});
 
-        })
-        res.status(201).send("Perro eliminado con éxito")
-    }catch (error){
-        res.status(404).send(`Error al eliminar: ${error}`)
-    }
-})
-
-
-
-module.exports = router
+module.exports = router;
--- api/src/routes/toShowApi.js
+++ api/src/routes/toShowApi.js
@@ -1,64 +1,63 @@
 //const { Router } = require('express');
-const {API_KEY}= process.env
-const { Dog, Temper} = require('../db')
-const axios = require('axios')
-const express = require('express')
+const { API_KEY } = process.env;
+const { Dog, Temper } = require("../db");
+const axios = require("axios");
+const express = require("express");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 // const router = Router();
 
 const urlDogs = `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`;
 const getDoguesito = async () => {
-    const dogData = await axios.get(urlDogs);
+  const dogData = await axios.get(urlDogs);
 
-    const dogInfo = await dogData.data.map((elem) => {
-        let temperamentArray = []; 
-        if(elem.temperament) {
-            temperamentArray = elem.temperament.split(", ")
-        }
+  const dogInfo = await dogData.data.map((elem) => {
+    let temperamentArray = [];
+    if (elem.temperament) {
+      temperamentArray = elem.temperament.split(", ");
+    }
 
-        let heightArray = [];
-        if(elem.height) {
-            heightArray = elem.height.metric.split(" - ")
-        }
+    let heightArray = [];
+    if (elem.height) {
+      heightArray = elem.height.metric.split(" - ");
+    }
 
-        let weightArray = [];
-        if(elem.weight) {
-            weightArray = elem.weight.metric.split(" - ")
-        }
+    let weightArray = [];
+    if (elem.weight) {
+      weightArray = elem.weight.metric.split(" - ");
+    }
     return {
-        id: elem.id,
-        name:elem.name,
-        height: heightArray,
-        weight: weightArray,
-        years: elem.life_span,
-        temperaments: temperamentArray,
-        image: elem.image.url
-      }
-    })
-    return dogInfo
-}
+      id: elem.id,
+      name: elem.name,
+      height: heightArray,
+      weight: weightArray,
+      years: elem.life_span,
+      temperaments: temperamentArray,
+      image: elem.image.url,
+    };
+  });
+  return dogInfo;
+};
 
 const getDogTemperDB = async () => {
-    return await Dog.findAll({
-        include: {
-            model: Temper,
-            attributes: ["name"],
-            trough: {
-                attributes: []
-            }
-        }
-    })
-}
+  return await Dog.findAll({
+    include: {
+      model: Temper,
+      attributes: ["name"],
+      trough: {
+        attributes: [],
+      },
+    },
+  });
+};
 
 const getAllDogs = async () => {
-    const infoApi = await getDoguesito();
-    const infoDB = await getDogTemperDB();
-    const infoDBApi = [...infoApi, ...infoDB];
-    return infoDBApi;
-}
+  const infoApi = await getDoguesito();
+  const infoDB = await getDogTemperDB();
+  const infoDBApi = [...infoApi, ...infoDB];
+  return infoDBApi;
+};
 
-module.exports = {getAllDogs};
+module.exports = { getAllDogs };
